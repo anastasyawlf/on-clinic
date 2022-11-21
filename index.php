9eb14678682ff127267cbabdeddf7fa255463687
anastasyawlf <najya.anastasya275@gmail.com>
2022-11-21 14:25:09 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SISTEM INFORMASI NILAI</title>
+  <link href="style.css" rel="stylesheet" />
+</head>
+<?php
+if (isset($_GET['error'])) { ?>
+  <p class="error"><?php echo $_GET['error']; ?></p>
+<?php } ?>
+
+<body class=" loginscroll background formbody">
+  <div>
+    <div class="shape"></div>
+    <div class="shape"></div>
+  </div>
+  <div class="loginform">
+    <form action="cek_level.php" method="post">
+      <h3 class="formh3">Login</h3>
+      <div>
+        <label class="loginlabel">NIM/NIP</label>
+        <input class="logininput" type="text" name="username" placeholder="NIM/NIP">
+      </div>
+      <div>
+        <label class="loginlabel">Password</label>
+        <input class="logininput" type="password" name="password" placeholder="Password">
+      </div>
+      <div>
+        <button class="loginbutton" type="submit">Log In</button>
+      </div>
+    </form>
+  </div>
+</body>
+
+</html>
