faf4672a16c146897f58465182b60b8758a9f673
mayyahsor <mayyahsor@gmail.com>
2022-11-29 16:11:03 +0700

    Update signup.php

--- doctor/signup.php
+++ doctor/signup.php
@@ -50,7 +50,7 @@ echo "<script>alert('Email-id already exist. Please try again');</script>";
 <html lang="en">
 <head>
 	
-	<title>DAMS - Login Page</title>
+	<title>On-Clinic - Login Page</title>
 	
 
 	<link rel="stylesheet" href="libs/bower/font-awesome/css/font-awesome.min.css">
@@ -69,11 +69,11 @@ echo "<script>alert('Email-id already exist. Please try again');</script>";
 		<div class="simple-page-logo animated swing">
 			
 				<span style="color: white"><i class="fa fa-gg"></i></span>
-				<span style="color: white">DAMS</span>
+				<span style="color: white">On-Clinic</span>
 			
 		</div><!-- logo -->
 		<div class="simple-page-form animated flipInY" id="login-form">
-	<h4 class="form-title m-b-xl text-center">Sign Up With Your DAMS Account</h4>
+	<h4 class="form-title m-b-xl text-center">Sign Up With Your On-CLinic Account</h4>
 	<form action="" method="post">
 		<div class="form-group">
 			<input id="fname" type="text" class="form-control" placeholder="Full Name" name="fname" required="true">
