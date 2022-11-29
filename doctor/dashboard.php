687f5582685cb8386368aa6eca9e56d8f0224210
anastasyawlf <najya.anastasya275@gmail.com>
2022-11-29 16:53:08 +0700

    benerin dashboard

--- doctor/dashboard.php
+++ doctor/dashboard.php
@@ -1,20 +1,17 @@
 <?php
 session_start();
-error_reporting(0);
+// error_reporting(0);
 include('includes/dbconnection.php');
-if (strlen($_SESSION['damsid']==0)) {
-  header('location:logout.php');
-  } else{
+var_dump($_SESSION['damsemailid']);
+?>
 
-
-
-  ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	
-	<title>DAMS - Dashboard</title>
-	
+
+	<title>On-Clinic - Dashboard</title>
+
 	<link rel="stylesheet" href="libs/bower/font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" href="libs/bower/material-design-iconic-font/dist/css/material-design-iconic-font.css">
 	<!-- build:css assets/css/app.min.css -->
@@ -31,141 +28,141 @@ if (strlen($_SESSION['damsid']==0)) {
 		Breakpoints();
 	</script>
 </head>
-	
+
 <body class="menubar-left menubar-unfold menubar-light theme-primary">
-<!--============= start main area -->
+	<!--============= start main area -->
 
 
- <?php include_once('includes/header.php');?>
+	<?php include_once('includes/header.php'); ?>
 
-<?php include_once('includes/sidebar.php');?>
+	<?php include_once('includes/sidebar.php'); ?>
 
 
 
-<!-- APP MAIN ==========-->
-<main id="app-main" class="app-main">
-  <div class="wrap">
-	<section class="app-content">
-		<div class="row">
-			
-		<div class="row">
-			<div class="col-md-6 col-sm-6">
-				<div class="widget stats-widget">
-					<div class="widget-body clearfix">
-						<?php 
-						 $docid=$_SESSION['damsid'];;
-$sql ="SELECT * from  tblappointment where Status is null && Doctor=:docid ";
-$query = $dbh -> prepare($sql);
-$query-> bindParam(':docid', $docid, PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$totnewapt=$query->rowCount();
-?>
-						<div class="pull-left">
-							<h3 class="widget-title text-warning"><span class="counter" data-plugin="counterUp"><?php echo htmlentities($totnewapt);?></span></h3>
-							<small class="text-color">Total New Appointment</small>
+	<!-- APP MAIN ==========-->
+	<main id="app-main" class="app-main">
+		<div class="wrap">
+			<section class="app-content">
+				<div class="row">
+
+					<div class="row">
+						<div class="col-md-6 col-sm-6">
+							<div class="widget stats-widget">
+								<div class="widget-body clearfix">
+									<?php
+									$docid = $_SESSION['damsid'];;
+									$sql = "SELECT * from  tblappointment where Status is null && Doctor=:docid ";
+									$query = $dbh->prepare($sql);
+									$query->bindParam(':docid', $docid, PDO::PARAM_STR);
+									$query->execute();
+									$results = $query->fetchAll(PDO::FETCH_OBJ);
+									$totnewapt = $query->rowCount();
+									?>
+									<div class="pull-left">
+										<h3 class="widget-title text-warning"><span class="counter" data-plugin="counterUp"><?php echo htmlentities($totnewapt); ?></span></h3>
+										<small class="text-color">Total New Appointment</small>
+									</div>
+									<span class="pull-right big-icon watermark"><i class="fa fa-paperclip"></i></span>
+								</div>
+								<footer class="widget-footer bg-warning">
+									<a href="new-appointment.php"><small> View Detail</small></a>
+									<span class="small-chart pull-right" data-plugin="sparkline" data-options="[4,3,5,2,1], { type: 'bar', barColor: '#ffffff', barWidth: 5, barSpacing: 2 }"></span>
+								</footer>
+							</div><!-- .widget -->
 						</div>
-						<span class="pull-right big-icon watermark"><i class="fa fa-paperclip"></i></span>
-					</div>
-					<footer class="widget-footer bg-warning">
-						<a href="new-appointment.php"><small> View Detail</small></a>
-						<span class="small-chart pull-right" data-plugin="sparkline" data-options="[4,3,5,2,1], { type: 'bar', barColor: '#ffffff', barWidth: 5, barSpacing: 2 }"></span>
-					</footer>
-				</div><!-- .widget -->
-			</div>
 
-			<div class="col-md-6 col-sm-6">
-				<div class="widget stats-widget">
-					<div class="widget-body clearfix">
-						<?php 
-						 $docid=$_SESSION['damsid'];;
-$sql ="SELECT * from  tblappointment where Status='Approved' && Doctor=:docid ";
-$query = $dbh -> prepare($sql);
-$query-> bindParam(':docid', $docid, PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$totappapt=$query->rowCount();
-?>
-						<div class="pull-left">
-							<h3 class="widget-title text-success"><span class="counter" data-plugin="counterUp"><?php echo htmlentities($totappapt);?></span></h3>
-							<small class="text-color">Total Approved</small>
+						<div class="col-md-6 col-sm-6">
+							<div class="widget stats-widget">
+								<div class="widget-body clearfix">
+									<?php
+									$docid = $_SESSION['damsid'];;
+									$sql = "SELECT * from  tblappointment where Status='Approved' && Doctor=:docid ";
+									$query = $dbh->prepare($sql);
+									$query->bindParam(':docid', $docid, PDO::PARAM_STR);
+									$query->execute();
+									$results = $query->fetchAll(PDO::FETCH_OBJ);
+									$totappapt = $query->rowCount();
+									?>
+									<div class="pull-left">
+										<h3 class="widget-title text-success"><span class="counter" data-plugin="counterUp"><?php echo htmlentities($totappapt); ?></span></h3>
+										<small class="text-color">Total Approved</small>
+									</div>
+									<span class="pull-right big-icon watermark"><i class="fa fa-ban"></i></span>
+								</div>
+								<footer class="widget-footer bg-success">
+									<a href="approved-appointment.php"><small> View Detail</small></a>
+									<span class="small-chart pull-right" data-plugin="sparkline" data-options="[1,2,3,5,4], { type: 'bar', barColor: '#ffffff', barWidth: 5, barSpacing: 2 }"></span>
+								</footer>
+							</div><!-- .widget -->
 						</div>
-						<span class="pull-right big-icon watermark"><i class="fa fa-ban"></i></span>
-					</div>
-					<footer class="widget-footer bg-success">
-						<a href="approved-appointment.php"><small> View Detail</small></a>
-						<span class="small-chart pull-right" data-plugin="sparkline" data-options="[1,2,3,5,4], { type: 'bar', barColor: '#ffffff', barWidth: 5, barSpacing: 2 }"></span>
-					</footer>
-				</div><!-- .widget -->
-			</div>
 
-			<div class="col-md-6 col-sm-6">
-				<div class="widget stats-widget">
-					<div class="widget-body clearfix">
-						<div class="pull-left">
-							<?php 
-						 $docid=$_SESSION['damsid'];;
-$sql ="SELECT * from  tblappointment where Status='Cancelled' && Doctor=:docid ";
-$query = $dbh -> prepare($sql);
-$query-> bindParam(':docid', $docid, PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$totncanapt=$query->rowCount();
-?>
-							<h3 class="widget-title text-danger"><span class="counter" data-plugin="counterUp"><?php echo htmlentities($totncanapt);?></span></h3>
-							<small class="text-color">Cancelled Appointment</small>
+						<div class="col-md-6 col-sm-6">
+							<div class="widget stats-widget">
+								<div class="widget-body clearfix">
+									<div class="pull-left">
+										<?php
+										$docid = $_SESSION['damsid'];;
+										$sql = "SELECT * from  tblappointment where Status='Cancelled' && Doctor=:docid ";
+										$query = $dbh->prepare($sql);
+										$query->bindParam(':docid', $docid, PDO::PARAM_STR);
+										$query->execute();
+										$results = $query->fetchAll(PDO::FETCH_OBJ);
+										$totncanapt = $query->rowCount();
+										?>
+										<h3 class="widget-title text-danger"><span class="counter" data-plugin="counterUp"><?php echo htmlentities($totncanapt); ?></span></h3>
+										<small class="text-color">Cancelled Appointment</small>
+									</div>
+									<span class="pull-right big-icon watermark"><i class="fa fa-unlock-alt"></i></span>
+								</div>
+								<footer class="widget-footer bg-danger">
+									<a href="cancelled-appointment.php"><small> View Detail</small></a>
+									<span class="small-chart pull-right" data-plugin="sparkline" data-options="[2,4,3,4,3], { type: 'bar', barColor: '#ffffff', barWidth: 5, barSpacing: 2 }"></span>
+								</footer>
+							</div><!-- .widget -->
 						</div>
-						<span class="pull-right big-icon watermark"><i class="fa fa-unlock-alt"></i></span>
-					</div>
-					<footer class="widget-footer bg-danger">
-						<a href="cancelled-appointment.php"><small> View Detail</small></a>
-						<span class="small-chart pull-right" data-plugin="sparkline" data-options="[2,4,3,4,3], { type: 'bar', barColor: '#ffffff', barWidth: 5, barSpacing: 2 }"></span>
-					</footer>
-				</div><!-- .widget -->
-			</div>
 
-			<div class="col-md-6 col-sm-6">
-				<div class="widget stats-widget">
-					<div class="widget-body clearfix">
+						<div class="col-md-6 col-sm-6">
+							<div class="widget stats-widget">
+								<div class="widget-body clearfix">
 
-						<div class="pull-left">
-							<?php 
-						 $docid=$_SESSION['damsid'];;
-$sql ="SELECT * from  tblappointment where Doctor=:docid ";
-$query = $dbh -> prepare($sql);
-$query-> bindParam(':docid', $docid, PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$totapt=$query->rowCount();
-?>
-							<h3 class="widget-title text-primary"><span class="counter" data-plugin="counterUp"><?php echo htmlentities($totapt);?></span></h3>
-							<small class="text-color">Total Appointment</small>
+									<div class="pull-left">
+										<?php
+										$docid = $_SESSION['damsid'];;
+										$sql = "SELECT * from  tblappointment where Doctor=:docid ";
+										$query = $dbh->prepare($sql);
+										$query->bindParam(':docid', $docid, PDO::PARAM_STR);
+										$query->execute();
+										$results = $query->fetchAll(PDO::FETCH_OBJ);
+										$totapt = $query->rowCount();
+										?>
+										<h3 class="widget-title text-primary"><span class="counter" data-plugin="counterUp"><?php echo htmlentities($totapt); ?></span></h3>
+										<small class="text-color">Total Appointment</small>
+									</div>
+									<span class="pull-right big-icon watermark"><i class="fa fa-file-text-o"></i></span>
+								</div>
+								<footer class="widget-footer bg-primary">
+									<a href="all-appointment.php"><small> View Detail</small></a>
+									<span class="small-chart pull-right" data-plugin="sparkline" data-options="[5,4,3,5,2],{ type: 'bar', barColor: '#ffffff', barWidth: 5, barSpacing: 2 }"></span>
+								</footer>
+							</div><!-- .widget -->
 						</div>
-						<span class="pull-right big-icon watermark"><i class="fa fa-file-text-o"></i></span>
-					</div>
-					<footer class="widget-footer bg-primary">
-						<a href="all-appointment.php"><small> View Detail</small></a>
-						<span class="small-chart pull-right" data-plugin="sparkline" data-options="[5,4,3,5,2],{ type: 'bar', barColor: '#ffffff', barWidth: 5, barSpacing: 2 }"></span>
-					</footer>
-				</div><!-- .widget -->
-			</div>
-		</div><!-- .row -->
+					</div><!-- .row -->
 
 
 
-		<div class="row">
-		
-	</section><!-- #dash-content -->
-</div><!-- .wrap -->
-  <!-- APP FOOTER -->
- <?php include_once('includes/footer.php');?>
-  <!-- /#app-footer -->
-</main>
-<!--========== END app main -->
+					<div class="row">
 
-<?php include_once('includes/customizer.php');?>
-	
-	
+			</section><!-- #dash-content -->
+		</div><!-- .wrap -->
+		<!-- APP FOOTER -->
+		<?php include_once('includes/footer.php'); ?>
+		<!-- /#app-footer -->
+	</main>
+	<!--========== END app main -->
+
+	<?php include_once('includes/customizer.php'); ?>
+
+
 
 	<!-- build:js assets/js/core.min.js -->
 	<script src="libs/bower/jquery/dist/jquery.js"></script>
@@ -186,5 +183,11 @@ $totapt=$query->rowCount();
 	<script src="libs/bower/fullcalendar/dist/fullcalendar.min.js"></script>
 	<script src="assets/js/fullcalendar.js"></script>
 </body>
+
 </html>
-<?php }  ?>
+<!-- if (strlen($_SESSION['damsid'] == 0)) {
+	header('location:logout.php');
+} else {
+?> -->
+
+<!-- <?php   ?> -->
--- doctor/includes/header.php
+++ doctor/includes/header.php
@@ -19,7 +19,7 @@
 
     <a href="dashboard.php" class="navbar-brand">
       <span class="brand-icon"><i class="fa fa-gg"></i></span>
-      <span class="brand-name">DAMS</span>
+      <span class="brand-name">On-Clinic</span>
     </a>
   </div><!-- .navbar-header -->
 
